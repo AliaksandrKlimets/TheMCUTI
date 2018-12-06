7aa894c4d81465f068b9144b337d62a40742c45d
Chizh <ivan.forever91@gmail.com>
2018-12-06 12:42:58 +0300

    Rename package

--- src/main/java/com/iba/TheMcutiApplication.java
+++ src/main/java/com/iba/TheMcutiApplication.java
@@ -1,4 +1,4 @@
-package com.iba.TheMCUTI;
+package com.iba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/iba/TheMcutiApplicationTests.java
+++ src/test/java/com/iba/TheMcutiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.iba.TheMCUTI;
+package com.iba;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
