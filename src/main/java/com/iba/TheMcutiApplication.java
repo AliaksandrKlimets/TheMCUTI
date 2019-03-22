026ed1fb0ca4c94501544b77cb52f5e4b729a124
Chizh <ivan.forever91.gmail.com>
2019-03-22 12:05:25 +0300

    test pull request 3

--- src/main/java/com/iba/TheMcutiApplication.java
+++ src/main/java/com/iba/TheMcutiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheMcutiApplication {
-	private String help;
 	public static void main(String[] args) {
 		SpringApplication.run(TheMcutiApplication.class, args);
 	}
